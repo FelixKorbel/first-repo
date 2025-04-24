cc5f85245130c3432ae00c71690cbb4c53969207
FelixKorbel <felix@korbel.ag>
2025-04-24 13:53:25 +0200

    mde a new txt out off a py

--- python/Task.py
+++ python/Task.py
@@ -1,3 +1,12 @@
 print("life-science-experiments on CubeSats")
 print("what ist possible? What is not possible?")
 print("What parameters are needed?")
+
+file_path = "exemple.txt"
+
+try: 
+    with open(file_path, 'x') as file:
+        file.write("life_science_experimets on CubeSats. What is possible? What is not possible?"
+                  "What parameters are needed?")
+except FileExistsError:
+    print("the file '{file_path}' already exists")

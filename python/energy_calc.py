b7ca9122cf49aa7dd2bcf65006a1f0edc5121e38
FelixKorbel <felix@korbel.ag>
2025-04-24 15:29:57 +0200

    calculations

--- python/energy_calc.py
+++ python/energy_calc.py
@@ -1,4 +1,27 @@
 # REad from yaml file
 # Calculate KE and PE
 # Output result to console
-vel =10
+import math
+import yaml 
+
+with open ("params.yaml", "r") as file : 
+    data= yaml.safe_load(file)
+
+
+v = data["vel"]
+m = data ["mass"]
+g = data ["grav"]
+#calculate e_kin
+e_kin = 1/2 * m * v**2
+
+#calculate e_pot
+e_pot = m * g * h
+
+#calculate total energy 
+e_t = e_kin + e_pot
+
+print(e_kin,"kinetic energy")
+print(e_pot,"potential energy")
+print(e_t,"total energy")
+      
+
